568746e9b58c504d96d16da536aa084a00d31d2e
Xteal <kevinra8@gmail.com>
2025-06-09 19:27:16 +0200

    Fix revalidation route slug

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -1,11 +1,14 @@
-export default async function handler(req, res) {  
-    try {
-      await res.revalidate('/');
-      await res.revalidate('/[slug]');
-      return res.json({ revalidated: true });
-    } catch (err) {
-      // If there was an error, Next.js will continue
-      // to show the last successfully generated page
-      return res.status(500).send('Error revalidating');
+export default async function handler(req, res) {
+  try {
+    const { slug } = req.query;
+    await res.revalidate('/');
+    if (slug) {
+      await res.revalidate(`/juego/${slug}`);
     }
+    return res.json({ revalidated: true });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating');
   }
+}
